920fa3f9db56ad836f88fc36a9b9da652e1dfa4c
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-20 03:40:02 -0600

    Update Main.java

--- Curso_de_Java_SE_Orientado_a_Objetos/03_CreandoElementosEstáticos/Main.java
+++ Curso_de_Java_SE_Orientado_a_Objetos/03_CreandoElementosEstáticos/Main.java
@@ -3,10 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Anahí Salgado");
-        myDoctor.name = "Alejandro Rodríguez";
-        myDoctor.showName();
-
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
