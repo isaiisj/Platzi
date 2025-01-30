58d34d9b59df759c691d797dc9b0331809d77f50
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-29 23:32:54 -0600

    Create Patient.java

---
+++ Curso_de_Java_SE_Orientado_a_Objetos/06_ModificadoresDeAcceso/Patient.java
@@ -0,0 +1,16 @@
+public class Patient {
+    //Atributos
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    private double weight;
+    private double height;
+    String blood;
+
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
