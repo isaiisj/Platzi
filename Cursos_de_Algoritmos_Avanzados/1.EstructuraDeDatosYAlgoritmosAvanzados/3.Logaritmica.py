84edde0acea3f0bd1fb1a8083a306060cf957eb8
Isai <47548422+isaiisj@users.noreply.github.com>
2025-04-01 11:29:16 -0600

    Create 3.Logaritmica.py

---
+++ Cursos_de_Algoritmos_Avanzados/1.EstructuraDeDatosYAlgoritmosAvanzados/3.Logaritmica.py
@@ -0,0 +1,13 @@
+def busqueda_binaria(lista, target):
+    apuntador_izquierdo = 0
+    apuntador_derecho = len(lista) - 1 
+    
+    while apuntador_izquierdo <= apuntador_derecho:
+        mitad = (apuntador_izquierdo+apuntador_derecho) // 2
+        if lista[mitad] == target:
+            return mitad
+        elif lista[mitad] < target:
+            apuntador_izquierdo = mitad + 1
+        else: 
+            apuntador_derecho = mitad - 1
+    return -1
