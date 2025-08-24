87185ec50d6792e359699ecdf9f537c2e6c1730d
Isai <47548422+isaiisj@users.noreply.github.com>
2025-08-24 02:42:24 -0600

    Create Usuario.java

---
+++ Curso_de_Java/PlatziPlay/platzi.play/Usuario.java
@@ -0,0 +1,38 @@
+package platzi.play.plataforma;
+
+import platzi.play.contenido.Contenido;
+
+import java.time.LocalDateTime;
+
+public class Usuario {
+    private String nombre;
+    private String email;
+    private LocalDateTime fechaRegistro;
+
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+        this.fechaRegistro = LocalDateTime.now();
+    }
+
+    public void ver(Contenido pelicula) {
+        System.out.println(nombre + " está viendo...");
+        pelicula.reproducir();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
