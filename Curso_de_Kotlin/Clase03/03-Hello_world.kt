94697def3eda3a6e77a0d997637d165c30fdc7bd
Isai <47548422+isaiisj@users.noreply.github.com>
2025-09-05 01:38:32 -0600

    Create 03-Hello_world.kt

---
+++ Curso_de_Kotlin/Clase03/03-Hello_world.kt
@@ -0,0 +1,25 @@
+package clase3
+
+fun main(){
+
+    println("¡Bienvenido a tu bandeja de entrada de Email!")
+
+    print("Sistema iniciado...")
+
+    println("¡Listo!")
+
+    println("\n=== EJERCICIO ===")
+    println("Crea tu propio mensaje de bienvenida que incluya:")
+    println("1. Tu nombre")
+    println("2. El nombre de tu aplicación de email")
+    println("3. Un mensaje motivacional")
+    println("4. Usa al menos 2 println() diferentes")
+
+
+    println("¡Hola! Soy Juan")
+    println("Bienvenido a P-mail")
+    println("¡Vamos a aprender Kotlin juntos!")
+    println("¡El futuro es ahora!")
+
+
+}
